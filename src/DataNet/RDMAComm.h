3829c2735f8d217014718b0c1f34f6000cc3f5c6
root <root@r9.asicdesigners.com>
2016-04-14 09:10:04 -0700

    Update the src files for licensing requirements
    Add this to each file changed to enable iWARP:
    
     ** This file has been modified by Chelsio Communications to enable iWARP.
     ** For any queries regarding the changes made, please contact
     ** support@chelsio.com<mailto:support@chelsio.com>.
    
    Signed-off-by: Steve Wise <swise@opengridcomputing.com>

--- src/DataNet/RDMAComm.h
+++ src/DataNet/RDMAComm.h
@@ -14,6 +14,9 @@
  ** either express or implied. See the License for the specific language
  ** governing permissions and  limitations under the License.
  **
+ ** This file has been modified by Chelsio Communications to enable iWARP.
+ ** For any queries regarding the changes made, please contact
+ ** support@chelsio.com<mailto:support@chelsio.com>.
  **
  */
 
